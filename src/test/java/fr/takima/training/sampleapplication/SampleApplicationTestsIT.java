cfad66b2195d7bae92695076f0177108fe81f981
Jonathan Le Bloas <jonathan.le_bloas@insa-rouen.fr>
2019-01-05 19:44:55 +0100

    fix: Remove spring usage in unit tests

--- src/test/java/fr/takima/training/sampleapplication/SampleApplicationTestsIT.java
+++ src/test/java/fr/takima/training/sampleapplication/SampleApplicationTestsIT.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SampleApplicationTests {
+public class SampleApplicationTestsIT {
 
 	@Test
 	public void contextLoads() {
--- src/test/java/fr/takima/training/sampleapplication/unit/DepartmentsServiceTest.java
+++ src/test/java/fr/takima/training/sampleapplication/unit/DepartmentsServiceTest.java
@@ -1,34 +1,18 @@
 package fr.takima.training.sampleapplication.unit;
 
-import fr.takima.training.sampleapplication.SampleApplication;
 import fr.takima.training.sampleapplication.dao.DepartmentDAO;
-import fr.takima.training.sampleapplication.dao.StudentDAO;
 import fr.takima.training.sampleapplication.entity.Department;
-import fr.takima.training.sampleapplication.entity.Student;
 import fr.takima.training.sampleapplication.service.DepartmentService;
-import fr.takima.training.sampleapplication.service.StudentService;
-import org.apache.catalina.startup.ContextConfig;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
-@ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes = ContextConfig.class)
-@SpringBootTest(classes = SampleApplication.class)
 public class DepartmentsServiceTest {
 
     @InjectMocks
